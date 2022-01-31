519c234ce3336c34a142a8cdb034807d36d5c074
annkis <anna.kishova@mail.ru>
2022-01-31 17:13:32 +0400

    Тест для разных языков интерфейса

---
+++ test_items.py
@@ -0,0 +1,12 @@
+from selenium import webdriver
+import pytest
+import time
+ 
+
+link = "http://selenium1py.pythonanywhere.com/catalogue/coders-at-work_207/"
+
+def test_1(browser):
+        browser.get(link)
+        
+        #наличие кнопки добавления в корзину
+        assert browser.find_element_by_css_selector(".btn-add-to-basket"), "Кнопка не найдена"
